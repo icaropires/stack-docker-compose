f1bfe78ec1db6a8b255fee93e4471d0fe123a75d
Ícaro Pires <icaropsa@gmail.com>
2021-02-01 17:46:04 -0300

    Expose metrics for flask app

--- app-python/app.py
+++ app-python/app.py
@@ -1,14 +1,27 @@
-import os, pika
+import os
+import pika
 from flask import Flask, request, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
+
+DEBUG = True
+
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+metrics.info('app_devops', 'Information from application devops demonstration')
 
 host = os.getenv("RABBITMQ_HOST", "localhost")
 port = os.getenv("RABBITMQ_PORT", 5672)
 queue = os.getenv("RABBITMQ_QUEUE", "hello")
 
-html = """ 
-<br>Type your favourite <i>pudim</i> flavour: 
+# Avoiding bug when using __main__
+#   https://github.com/rycus86/prometheus_flask_exporter/issues/40
+os.environ["DEBUG_METRICS"] = "true" if DEBUG else "false"
+
+html = """
+<br>Type your favourite <i>pudim</i> flavour:
 <br>
 <form method='POST' action='/'>
     <input type='text' name='flavour'>
@@ -41,5 +54,6 @@ def enqueue(value):
     app.logger.info("Enqueued message on host %s:%s queue %s: %s", host, port,
                     queue, value)
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=DEBUG, host="0.0.0.0", port=8000)
